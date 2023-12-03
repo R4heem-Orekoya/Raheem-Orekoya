c6cb8d11ca40aa1c98a17c3f49b0973c653454f8
Raheem Orekoya <tundeorekoya1@gmail.com>
2023-12-03 04:04:22 +0100

    fixed padding issues

--- src/Pages/BlogPage.jsx
+++ src/Pages/BlogPage.jsx
@@ -57,7 +57,7 @@ const BlogPage = () => {
   }
 
   return (
-    <section className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-16'>
+    <section className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 py-16'>
       {blogs.map((blog, index) => (
         <BlogCard blog={blog} key={index}/>
       ))}
